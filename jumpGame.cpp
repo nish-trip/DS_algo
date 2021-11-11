da4c2f97921a50a27ce64003857ba5516566f14e
Nishant Tripathy <tripathynishant@gmail.com>
2021-11-11 12:49:20 +0530

    Create jumpGame.cpp

---
+++ jumpGame.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int cur = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(i>cur){
+                return false;
+            }
+            cur = max(cur, i+nums[i]);
+        }      
+        return true;
+    }
+};
