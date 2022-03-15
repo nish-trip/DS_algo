f54df4aad43a903cb8cbc933d5416cd7b8788412
nishant tripathy <ntripathy@MacBook-Pro.local>
2022-03-15 13:37:06 +0530

    using max heap to store the first half and minheap to store the 2nd half of the array

--- findMedianInStream.cpp
+++ findMedianInStream.cpp
@@ -2,58 +2,44 @@
 
 class MedianFinder {
 public:
-    priority_queue<int>maxHeap;
-    priority_queue<int,vector<int>, greater<int>>minHeap;
+    priority_queue<int>maxheap;
+    priority_queue<int, vector<int>, greater<int>>minheap;
     MedianFinder() {}
     
     void addNum(int num) {
-        if(maxHeap.size() == 0){
-            maxHeap.push(num);
+        if(maxheap.empty()){    // first element
+            maxheap.push(num);
         }
-        else if(maxHeap.size() == minHeap.size()){
-            if(num<minHeap.top()){
-                maxHeap.push(num);
+
+        else if(maxheap.size() == minheap.size()){      // if the size is same, insert to maxheap
+            if(num<=minheap.top()){
+                maxheap.push(num);
             }
             else{
-                int temp = minHeap.top();
-                minHeap.pop();
-                maxHeap.push(temp);
-                minHeap.push(num);
+                int temp = minheap.top();
+                minheap.pop();
+                maxheap.push(temp);
+                minheap.push(num);
             }
         }
-        else{
-            if(minHeap.size() == 0){
-                if(num>maxHeap.top()){
-                    minHeap.push(num);
-                }
-                else{
-                    int temp = maxHeap.top();
-                    maxHeap.pop();
-                    minHeap.push(temp);
-                    maxHeap.push(num);
-                }
-            }
-            else if(num>= minHeap.top()){
-                minHeap.push(num);
+
+        else{       // maheap.size()+1 == minheap.size(),   insert to minheap
+            if(num>= maxheap.top()){
+                minheap.push(num);
             }
             else{
-                if(num>maxHeap.top()){
-                    minHeap.push(num);
-                }
-                else{
-                    int temp = maxHeap.top();
-                    maxHeap.pop();
-                    minHeap.push(temp);
-                    maxHeap.push(num);
-                }
+                int temp = maxheap.top();
+                maxheap.pop();
+                minheap.push(temp);
+                maxheap.push(num);
             }
         }
     }
     
     double findMedian() {
-        if(maxHeap.size() == minHeap.size()){
-            return (maxHeap.top() + minHeap.top())/2;
+        if(maxheap.size() == minheap.size()){
+            return (double)(maxheap.top()+minheap.top())/2;
         }
-        return maxHeap.top();
+        return maxheap.top();
     }
 };
