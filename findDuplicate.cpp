9dec638dec43d0d9659cb7b3efa9d1ce16731106
nishant tripathy <ntripathy@MacBook-Pro.local>
2022-03-22 12:59:57 +0530

    using cycle detection

--- findDuplicate.cpp
+++ findDuplicate.cpp
@@ -1,39 +1,19 @@
-#include<bits/stdc++.h>
+#include "test.h"
 using namespace std;
-
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode() : val(0), left(nullptr), right(nullptr) {}
-	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {} 
-	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
-struct Node
-{
-	int data;
-	struct Node *next;
-};
-
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
 
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-		for(int i=0; i<nums.size();i++){
-			int temp = abs(nums[i]);
-			if(nums[temp-1]<0){
-				return temp;
-			}
-			nums[temp-1] *= -1;
+		int slow = nums[0], fast = nums[nums[0]];
+		while(slow != fast){
+			slow = nums[slow];
+			fast = nums[nums[fast]];
 		}
-		return -1;
+		slow = 0;
+		while(slow != fast){
+			slow = nums[slow];
+			fast = nums[fast];
+		}
+		return slow;
     }
 };
