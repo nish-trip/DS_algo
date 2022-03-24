d2129d1079e22db20f8f3321dc21ada7b6151a1a
nishant tripathy <ntripathy@MacBook-Pro.local>
2022-03-24 16:17:23 +0530

    atMost(k)-atMost(k-1) . variable sliding window to make sure the subarray has atmost k distinct integers, otherwise we shrink from left

--- arrayWithKdistinctInteger.cpp
+++ arrayWithKdistinctInteger.cpp
@@ -1,28 +1,26 @@
-#include<bits/stdc++.h>
+#include "test.h"
 using namespace std;
 
 class Solution {
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-		return atMost(nums,k)-atMost(nums, k-1); 
+        return atMost(nums, k)-atMost(nums, k-1);      
     }
 
-    int atMost(vector<int>& arr, int k)
-    {
-        int count = 0;
-        int left = 0;
-        unordered_map<int,int> map;
-        for(int right = 0; right<arr.size(); right++) {
-            map[arr[right]]++;
-            while (map.size() > k) {
-                map[arr[left]] --;
-                if (map[arr[left]] == 0){
-                    map.erase(arr[left]);
+    int atMost(vector<int>& nums, int k){
+        unordered_map<int,int>hash;
+        int start=0, ans=0;
+        for(int end=0; end<nums.size(); end++){
+            hash[nums[end]]++;
+            while(hash.size()>k){
+                hash[nums[start]]--;
+                if(hash[nums[start]] == 0){
+                    hash.erase(nums[start]);
                 }
-                left++;
+                start++;
             }
-            count += right - left + 1;
+            ans+= end-start+1;
         }
-        return count;
+        return ans;
     }
 };
