d18de1c9f5f878152cd3bdeb5b7674d8f5101c5d
nishant tripathy <ntripathy@MacBook-Pro.local>
2022-03-29 14:13:57 +0530

    find the left and right margin for each element such that the entire bar can be contained

--- maxRectangleHistogram.cpp
+++ maxRectangleHistogram.cpp
@@ -3,29 +3,33 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();      
+        // the largest rectangle contains atleast one of bar completely
+        int ans =0;
+        vector<int>left(heights.size()), right(heights.size());
         stack<int>st;
-        vector<int> left(n), right(n);
-        for(int i=0; i<n; i++){
-            while(!st.empty() && heights[st.top()]>= heights[i]){
-                st.pop();
-            }
-            left[i] = st.empty()?0: st.top()+1;
-            st.push(i);
-        }
-        while(!st.empty()){
-            st.pop();
-        }
-        for(int i=n-1; i>=0; i--){
+        // we iterate through the heights and the left most index wher ethis bar can be contained completely 
+        for(int i=0; i<heights.size(); i++){
             while(!st.empty() && heights[st.top()]>=heights[i]){
                 st.pop();
             }
-            right[i] = st.empty()? n-1: st.top()-1;
+            left[i] = st.empty()? 0: st.top()+1;
+            st.push(i);
+        }      
+        // we empty the stack
+        while(!st.empty()){
+            st.pop();
+        }
+        // iterate through the heights and find the rightmost index such that the current bar can be contained completely
+        for(int i=heights.size()-1; i>=0; i--){
+            while(!st.empty() && heights[st.top()]>=heights[i]){
+                st.pop();
+            }
+            right[i] = st.empty()? heights.size()-1: st.top()-1;
             st.push(i);
         }
-        int ans =0;
-        for(int i=0; i<n; i++){
-            ans = max(ans, (right[i]-left[i]+1)*heights[i]);
+        // iterate through the heights and find the max area possible 
+        for(int i=0; i<heights.size(); i++){
+            ans = max(ans, heights[i]*(right[i]-left[i]+1));
         }
         return ans;
     }
