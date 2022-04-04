59f2fc628de47a2181a1bbdff3f387d847c515fb
nishant tripathy <ntripathy@MacBook-Pro.local>
2022-04-04 13:14:16 +0530

    iterate the tail for len-k steps, the new head will be tail->next and then set the tail->next to NULL

--- rotateLinkedlist.cpp
+++ rotateLinkedlist.cpp
@@ -5,20 +5,17 @@ public:
     ListNode* rotateRight(ListNode* head, int k) {
         if(!head || !head->next){
             return head;
-        }
-        int len =1;
+        }      
+        int len = 1;
         ListNode* tail = head;
         while(tail->next){
             len++;
             tail = tail->next;
-        }      
-        k%= len;
-        if(!k){
-            return head;
         }
-        ListNode* newTail = tail;
-        int steps = len-k;
+        k%= len;
+        if(!k) return head;
         tail->next = head;
+        int steps = len-k;
         while(steps--){
             tail = tail->next;
         }
