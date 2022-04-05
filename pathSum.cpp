16c9bbcdbba74065111889d41c9ed5e622a8b1be
nishant tripathy <ntripathy@MacBook-Pro.local>
2022-04-05 14:00:54 +0530

    go down the tree recursively to see if all the conditions are matched

---
+++ pathSum.cpp
@@ -0,0 +1,16 @@
+#include "test.h"
+
+#include "test.h"
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(!root){
+            return false;
+        }      
+        if(!root->left && !root->right && root->val == targetSum){
+            return true;
+        }
+        return hasPathSum(root->left, targetSum-root->val) || hasPathSum(root->right, targetSum-root->val);
+    }
+};
