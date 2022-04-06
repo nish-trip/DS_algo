b8fb4b11cd7b93a17f19b261880db525284ca315
nishant tripathy <ntripathy@MacBook-Pro.local>
2022-04-06 18:34:04 +0530

    using graph dfs

---
+++ numberOfIslands.cpp
@@ -0,0 +1,28 @@
+#include "test.h"
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int ans=0, m=grid.size(), n=grid[0].size();
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == '1'){
+                    ans++;
+                    dfs(grid, i, j);
+                }
+            }
+        }      
+        return ans;
+    }
+
+    void dfs(vector<vector<char>>& grid, int i, int j){
+        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j] != '1'){
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i-1, j);
+        dfs(grid, i+1, j);
+        dfs(grid, i, j-1);
+        dfs(grid, i, j+1);
+    }
+};
