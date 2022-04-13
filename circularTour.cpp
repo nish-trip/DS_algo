0a198193fce412ee5b5e73d0b9e29fa87841953d
nishant tripathy <ntripathy@MacBook-Pro.local>
2022-04-13 19:55:26 +0530

    keeping track of the current petrol balance and the total deficit till the last invalid index

---
+++ circularTour.cpp
@@ -0,0 +1,18 @@
+#include "test.h"
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int deficit=0, start=0,tank=0;
+        for(int i=0;i<gas.size();i++){
+            tank+=gas[i]-cost[i];
+            if(tank<0){
+                start=i+1;
+                deficit+=tank;
+                tank=0;
+            }
+
+        }
+        return (tank+deficit)>=0 ? start :-1;
+    }
+};
