d576f376444fd4026636dcf3d59405e6165dde64
nishant tripathy <ntripathy@MacBook-Pro.local>
2022-04-13 15:55:45 +0530

    updated min falling path sum. using only constant space and updating the given matrix since we dont need the current matrix vals

--- minFallingPathSum.cpp
+++ minFallingPathSum.cpp
@@ -3,19 +3,12 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        if(n==1){
-            return *min_element(matrix[0].begin(), matrix[0].end());
-        }
-        vector<int>prev(n);
-        prev = matrix[0];
-        vector<int>cur(n);
-        for(int i=1; i<n; i++){
+        int m = matrix.size(), n=matrix[0].size();
+        for(int i=1; i<m; i++){
             for(int j=0; j<n; j++){
-                cur[j] = min(prev[max(0,j-1)]+matrix[i][j], min(prev[j]+matrix[i][j], prev[min(n-1, j+1)]+matrix[i][j]));
+                matrix[i][j] += min({ matrix[i-1][max(0, j-1)], matrix[i-1][j], matrix[i-1][min(n-1,j+1)] });
             }
-            prev = cur;
         }
-        return *min_element(cur.begin(), cur.end());
+        return *min_element(matrix[n-1].begin(), matrix[n-1].end());
     }
 };
