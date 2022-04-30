edb05aa21e170164981ce257a9f111bec02b2c56
nishant tripathy <ntripathy@MacBook-Pro.local>
2022-04-30 15:05:21 +0530

    using stack to keep trach of previous maximums and the stock span at that maximum

---
+++ stockSpan.cpp
@@ -0,0 +1,18 @@
+#include "test.h"
+
+class StockSpanner {
+public:
+    // {price, span};
+    stack<pair<int,int>>st;
+    StockSpanner() {}
+    
+    int next(int price) {
+        int span = 1;
+        while(!st.empty() && price>=st.top().first){
+            span+= st.top().second;
+            st.pop();
+        }
+        st.push(make_pair(price, span));
+        return span;
+    }
+};
