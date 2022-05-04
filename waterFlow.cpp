aab7776c92db28f1a8468e653b38ef67c64c32d7
nishant-infoblox <ntripathy@infoblox.com>
2022-05-04 23:27:07 +0530

    using dfs from the edges which give O(m*n) time complexity

---
+++ waterFlow.cpp
@@ -0,0 +1,38 @@
+#include "test.h"
+
+class Solution {
+public:
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& grid) {
+        vector<vector<int>>ans;
+        int rows = grid.size(), cols = grid[0].size();
+        set<pair<int,int>>pac, atl;
+        
+        for(int i=0; i<rows; i++){
+            dfs(grid, pac, i, 0, grid[i][0]);
+            dfs(grid, atl, i, cols-1, grid[i][cols-1]);
+        }
+        for(int i=0; i<cols; i++){
+            dfs(grid, pac, 0, i, grid[0][i]);
+            dfs(grid, atl, rows-1, i, grid[rows-1][i]);
+        }
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                if(pac.count({i,j}) && atl.count({i,j})){
+                    ans.push_back({i,j});
+                }
+            }
+        }
+        return ans;
+    }
+    
+    void dfs(vector<vector<int>>& grid, set<pair<int,int>>& s, int i, int j, int prev){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || prev>grid[i][j]){
+            return;
+        }
+        s.insert({i,j});
+        dfs(grid, s, i+1, j, grid[i][j]);
+        dfs(grid, s, i-1, j, grid[i][j]);
+        dfs(grid, s, i, j+1, grid[i][j]);
+        dfs(grid, s, i, j-1, grid[i][j]);
+    }
+};
